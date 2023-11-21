8531d4c731eff7482c67789a63a91d11adb591fe
Varad Gattani <gattanivarad@gmail.com>
2023-11-21 21:22:58 +0530

    track wala part

--- backend/models/FiledCase.js
+++ backend/models/FiledCase.js
@@ -1,90 +1,95 @@
-const mongoose = require('mongoose');
-const {ObjectId} = mongoose.Schema;
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
 
 // TODO
-const filedCaseSchema = new mongoose.Schema({
+const filedCaseSchema = new mongoose.Schema(
+  {
     category: {
-        type: String,
-        enum: ['civil', 'criminal'],
-        required: [true, 'case category is required!'],
+      type: String,
+      enum: ["civil", "criminal"],
+      required: [true, "case category is required!"],
     },
     caseType: {
-        type: String,
-        required: [true, 'case type is required!'],
+      type: String,
+      required: [true, "case type is required!"],
     },
     plaintiffDetails: {
-        plaintiff: {
-            name: {
-                type: String,
-                required: [true, 'plaintiff name is required!'],
-            },
-            gender: {
-                type: String,
-                enum: ['male', 'female', 'other'],
-                required: [true, 'plaintiff gender is required!'],
-            },
-            age: {
-                type: String,
-                required: [true, 'plaintiff age is required!'],
-            },
-            extraPetitionerCnt: String,
-            mobile: {
-                type: String,
-                required: [true, 'plaintiff mobile no. is required!'],
-            }
+      plaintiff: {
+        name: {
+          type: String,
+          required: [true, "plaintiff name is required!"],
         },
-        advocate: {
-            name: {
-                type: String,
-            },
-            barRegNum: {
-                type: String,
-            },
-            email: {
-                type: String,
-            }
+        gender: {
+          type: String,
+          enum: ["male", "female", "other"],
+          required: [true, "plaintiff gender is required!"],
         },
-        
+        age: {
+          type: String,
+          required: [true, "plaintiff age is required!"],
+        },
+        extraPetitionerCnt: String,
+        mobile: {
+          type: String,
+          required: [true, "plaintiff mobile no. is required!"],
+        },
+      },
+      advocate: {
+        name: {
+          type: String,
+        },
+        barRegNum: {
+          type: String,
+        },
+        email: {
+          type: String,
+        },
+      },
     },
     defendantDetails: {
-        defendant: {
-            name: {
-                type: String,
-                required: [true, 'defendant name is required!'],
-            },
-            gender: {
-                type: String,
-                enum: ['male', 'female', 'other'],
-                required: [true, 'defendant gender is required!'],
-            },
-            age: {
-                type: String,
-                required: [true, 'defendant age is required!'],
-            },
-            extraPetitionerCnt: String,
-            mobile: {
-                type: String,
-                required: [true, 'defendant mobile no. is required!'],
-            }
+      defendant: {
+        name: {
+          type: String,
+          required: [true, "defendant name is required!"],
         },
+        gender: {
+          type: String,
+          enum: ["male", "female", "other"],
+          required: [true, "defendant gender is required!"],
+        },
+        age: {
+          type: String,
+          required: [true, "defendant age is required!"],
+        },
+        extraPetitionerCnt: String,
+        mobile: {
+          type: String,
+          required: [true, "defendant mobile no. is required!"],
+        },
+      },
+    },
+    caseTrack: {
+      type: String,
     },
 
     valuation: {
-        type: String,
+      type: String,
     },
     amount: {
-        type: String,
+      type: String,
     },
     filingDateAndTime: {
-        type: Date,
+      type: Date,
     },
 
     filingNum: {
-        type: String,
+      type: String,
     },
     cnrNum: {
-        type: String,
-    }
-}, {timestamps: true});
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('FiledCase', filedCaseSchema);
+module.exports = mongoose.model("FiledCase", filedCaseSchema);
