b0b7e0b920d1be02bd860fed2b05159f36d1a088
IAmVengeance <ayan.saxena24@gmail.com>
2024-01-04 13:08:33 +0530

    Update axios.jsx

--- frontend/src/pages/api/axios.jsx
+++ frontend/src/pages/api/axios.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8000/api';
+// const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://nyaya-sanchalak-backend-s5pr.onrender.com/';
 
 export default axios.create({ 
     baseURL : BASE_URL
